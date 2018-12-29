4c09085bd7733a71c0e72989fd1d0bc7f9c3b171
solos <lxl1217@gmail.com>
2018-12-29 22:39:21 +0800

    add Range
    add Range

--- zset_test.go
+++ zset_test.go
@@ -1,6 +1,7 @@
 package zset
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -16,6 +17,8 @@ func TestNew(t *testing.T) {
 	}
 	s.Set(66.0, "test1", "")
 	s.Set(77.0, "test2", "")
+	s.Set(87.0, "test3", "")
+	s.Set(89.0, "test4", "")
 
 	rank, score, extra := s.GetRank("test1", false)
 	if rank == 0 {
@@ -29,5 +32,22 @@ func TestNew(t *testing.T) {
 	} else {
 		t.Error("Key:", "test2", "Rank:", rank, "Score:", score, "Extra:", extra)
 	}
-}
+	items := s.GetRange(uint64(1), uint64(2))
+	fmt.Println(items)
+	if items[0].Score != 77.0 {
+		t.Error("Key:", "test2")
+	}
+	items = s.GetRange(uint64(1), uint64(4))
+	fmt.Println(items)
+	if items[0].Score != 77.0 {
+		t.Error("Key:", "test2")
+	}
+	if items[1].Score != 87.0 {
+		t.Error("Key:", "test3")
+	}
 
+	if items[2].Score != 89.0 {
+		t.Error("Key:", "test4")
+	}
+
+}
